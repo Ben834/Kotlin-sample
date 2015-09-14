fe8d3db16a182a95f1dd6cce323837644a77ec61
Benjamin Duvivier <benjamin.duvivier@djit.fr>
2015-09-14 16:31:34 +0200

    Tutorial chapter 11

--- app/src/main/java/com/ben/kotlinsample/domain/model/DomainClasses.kt
+++ app/src/main/java/com/ben/kotlinsample/domain/model/DomainClasses.kt
@@ -1,5 +1,11 @@
 package com.ben.kotlinsample.domain.model
 
 /** The domain data classes **/
-data class ForecastList(val city: String, val country: String, val dailyForecast:List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast:List<Forecast>){
+
+    public fun get(position: Int) : Forecast = dailyForecast.get(position)
+    public fun size() : Int = dailyForecast.size()
+
+}
+
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
