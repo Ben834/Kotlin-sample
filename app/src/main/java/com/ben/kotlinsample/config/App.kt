0be56019c9a86f2d835eaa774231b9bd9e330d92
Benjamin Duvivier <benjamin.duvivier@djit.fr>
2015-09-16 16:51:25 +0200

    Tutorial chapter 16

---
+++ app/src/main/java/com/ben/kotlinsample/config/App.kt
@@ -0,0 +1,18 @@
+package com.ben.kotlinsample.config
+
+import android.app.Application
+import android.database.sqlite.SQLiteOpenHelper
+import kotlin.properties.Delegates
+
+/** Custom application **/
+class App: Application(){
+
+    companion object{
+        private var instance : Application by NotNullSingleValueVar()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
---
+++ app/src/main/java/com/ben/kotlinsample/config/DelegatesExt.kt
@@ -0,0 +1,19 @@
+package com.ben.kotlinsample.config
+
+import kotlin.properties.ReadWriteProperty
+
+/** Custom delegate properties **/
+private class NotNullSingleValueVar<T: Any>() : ReadWriteProperty<Any?, T>{
+
+    private var value: T? = null
+
+    override fun get(thisRef: Any?, desc: PropertyMetadata): T {
+        return value ?: throw IllegalStateException("Property ${desc.name} should be initialized before get")
+    }
+
+    override fun set(thisRef: Any?, desc: PropertyMetadata, value: T) {
+        this.value = if(this.value == null) value
+        else throw  IllegalStateException("${desc.name} already initialized")
+    }
+
+}
